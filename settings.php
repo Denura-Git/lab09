85da5f645069ea92585ea481a65ebec1c51a5fa6
Denura-Git <105562893@student.swin.edu.au>
2025-05-13 22:16:06 +1000

    settings php

---
+++ settings.php
@@ -0,0 +1,7 @@
+<?php
+// Database connection details
+$host = "localhost"; // MySQL server location (localhost if you are using XAMPP)
+$user = "root";      // Username for MySQL (default username for XAMPP is "root")
+$pwd = "";           // Password for MySQL (default password is empty for XAMPP)
+$sql_db = "exhibition_db"; // The name of the database you created earlier
+?>
